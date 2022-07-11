2f5a0f090901c92ec2d3c0dc9ed68b7698f9b3fc
MSM-WOWL <decevollily@gmail.com>
2022-07-11 17:18:35 +0900

    index・newページの作成

---
+++ db/migrate/20220709070947_create_stocks.rb
@@ -0,0 +1,12 @@
+class CreateStocks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stocks do |t|
+      t.string :area
+      t.string :store_name
+      t.string :product
+      t.string :lottery
+      t.text :comment
+      t.timestamps
+    end
+  end
+end
